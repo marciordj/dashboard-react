880cbdf0f3610dc6a55a0ba832fa42ea31ba1bf4
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-07-30 07:35:35 -0300

    feat: add react helmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import './global.css'
 import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | dashboard' />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
